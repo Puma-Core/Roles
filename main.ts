0c72bf4d16e7369ff54724166b7cb4c01a16483f
Veronelli <facu2000veronelli@outlook.com>
2026-06-03 01:01:18 +0100

    change port  name

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@ server.get("/health-check", async () => {
 });
 
 async function start(): Promise<void> {
-    const port = Number(process.env.PORT ?? 3000);
+    const port = Number(process.env.FASTIFY_PORT ?? 3000);
 
     await server.listen({ port, host: "0.0.0.0" });
 }
